517e76c52ad0b3719c73ee9f02b0a218ef0cb219
lolerwaffles <bear@ursimian.com>
2018-06-13 19:16:22 -0500

    IMEI and SN update
    Refactored SN algo, and added IMEI fetching for filesystem nameing.

--- logall.py
+++ logall.py
@@ -1,9 +1,9 @@
-import os, time, io
+import os, io, re, subprocess, datetime
+
 
 os.system('cls')
-print("Walter's LogAll v3, ver.0.1\n\n")
+print("Walter's LogAll v3, ver.0.1\nAutomatically Connects to all ADB enabled devices and logs them.\nA Folder named with IMEI and timestamp is created for each connected device\n")
 
-'''This is pretty fast and nasty, I need to make some of these if blocks into real functions, lol'''
 
 testCase = input("Test Case Number\n")
 
@@ -16,21 +16,26 @@ while devCheck == 'n' or devCheck == 'N':
 print("\n\nstarting logging")
 
 ADBrawInput = str(io.StringIO(os.popen(".\\platform-tools\\adb devices").read()).read())
-NumDevices = ADBrawInput.count("\tdevice")
+pattern = re.compile('\\n.*?\\t')
 
-for i in range(NumDevices):
-    if NumDevices == 1:
-        DUTadd = ADBrawInput[(ADBrawInput.find('\n') + 1):ADBrawInput.find('\t')]
-    else:
-        DUTadd = ADBrawInput[(ADBrawInput.find(DUTadd) + 2):ADBrawInput.find('\t')]
+DUTadd = pattern.findall(ADBrawInput)
 
-    DUTFolder = "{}_{}".format(DUTadd, testCase)
+for i in range(len(DUTadd)):
+    DUTadd[i] = DUTadd[i][1:-1]
+
+    IMEI = str(io.StringIO(os.popen( r'''.\\platform-tools\\adb -s {} shell "service call iphonesubinfo 1 | toybox cut -d \"'\" -f2 | toybox grep -Eo '[0-9]' | toybox xargs | toybox sed 's/\ //g'"'''.format(DUTadd[i])).read()).read())
+    IMEI = IMEI[:-1:]
+
+    DUTFolder = "{}_{}_{}".format(IMEI, testCase , datetime.datetime.now().strftime("%H-%M-%S"))
 
     os.mkdir('{}'.format(DUTFolder))
 
-    os.popen("start cmd /C .\\platform-tools\\adb -s {} logcat -b radio -v threadtime ^>^>{}/logradio.txt".format(DUTadd, DUTFolder))
-    os.popen("start cmd /C .\\platform-tools\\adb -s {} logcat -b main -v threadtime ^>^>{}/logmain.txt".format(DUTadd, DUTFolder))
-    os.popen("start cmd /C .\\platform-tools\\adb -s {} logcat -b kernel -v threadtime ^>^>{}/logkernel.txt".format(DUTadd, DUTFolder))
-    os.popen("start cmd /C .\\platform-tools\\adb -s {} logcat -b events -v threadtime ^>^>{}/logevents.txt".format(DUTadd, DUTFolder))
-    os.popen("start cmd /C .\\platform-tools\\adb -s {} logcat -b system -v threadtime ^>^>{}/logsystem.txt".format(DUTadd, DUTFolder))
-    os.popen("start cmd /C .\\platform-tools\\adb -s {} logcat -b all -v threadtime ^>^>{}/logall.txt".format(DUTadd, DUTFolder))
+    os.popen("start cmd /C .\\platform-tools\\adb -s {} logcat -b radio -v threadtime ^>^>{}/logradio.txt".format(DUTadd[i], DUTFolder))
+    os.popen("start cmd /C .\\platform-tools\\adb -s {} logcat -b main -v threadtime ^>^>{}/logmain.txt".format(DUTadd[i], DUTFolder))
+    os.popen("start cmd /C .\\platform-tools\\adb -s {} logcat -b kernel -v threadtime ^>^>{}/logkernel.txt".format(DUTadd[i], DUTFolder))
+    os.popen("start cmd /C .\\platform-tools\\adb -s {} logcat -b events -v threadtime ^>^>{}/logevents.txt".format(DUTadd[i], DUTFolder))
+    os.popen("start cmd /C .\\platform-tools\\adb -s {} logcat -b system -v threadtime ^>^>{}/logsystem.txt".format(DUTadd[i], DUTFolder))
+    os.popen("start cmd /C .\\platform-tools\\adb -s {} logcat -b all -v threadtime ^>^>{}/logall.txt".format(DUTadd[i], DUTFolder))
+
+
+print("Logging started")
