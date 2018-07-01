f95ec268aebc68c0761ac62d2da0daf8a51d3ec9
lolerwaffles <bear@ursimian.com>
2018-06-30 23:42:57 -0700

    device properties started

---
+++ deviceProperties.py
@@ -0,0 +1,45 @@
+import os,io,re
+
+dataTemp = str(io.StringIO(os.popen( r'''.\\platform-tools\\adb shell "service call iphonesubinfo 1 | toybox cut -d \"'\" -f2 | toybox grep -Eo '[0-9]' | toybox xargs | toybox sed 's/\ //g'"''').read()).read())
+rePattern = re.compile('[?!\\n]*(.*.)[\\t]*')
+print("IMEI:                 {}".format(rePattern.findall(dataTemp)[0]))
+
+dataTemp = str(io.StringIO(os.popen( r'''.\\platform-tools\\adb shell "service call iphonesubinfo 19 | toybox cut -d \"'\" -f2 | toybox grep -Eo '[0-9]' | toybox xargs | toybox sed 's/\ //g'"''').read()).read())
+rePattern = re.compile('[?!\\n]*(.*.)[\\t]*')
+print("MDN:                  {}".format(rePattern.findall(dataTemp)[0]))
+
+dataTemp = str(io.StringIO(os.popen( r'''.\\platform-tools\\adb shell "service call iphonesubinfo 8 | toybox cut -d \"'\" -f2 | toybox grep -Eo '[0-9]' | toybox xargs | toybox sed 's/\ //g'"''').read()).read())
+rePattern = re.compile('[?!\\n]*(.*.)[\\t]*')
+print("IMSI:                 {}".format(rePattern.findall(dataTemp)[0]))
+
+dataTemp = str(io.StringIO(os.popen( r'''.\\platform-tools\\adb shell getprop ro.vendor.product.brand''').read()).read())
+rePattern = re.compile('[?!\\n]*(.*.)[\\t]*')
+print("Device Brand:         {}".format(rePattern.findall(dataTemp)[0]))
+
+dataTemp = str(io.StringIO(os.popen( r'''.\\platform-tools\\adb shell getprop ro.vendor.product.name''').read()).read())
+rePattern = re.compile('[?!\\n]*(.*.)[\\t]*')
+print("Device Name:          {}".format(rePattern.findall(dataTemp)[0]))
+
+dataTemp = str(io.StringIO(os.popen( r'''.\\platform-tools\\adb shell getprop ro.build.display.id''').read()).read())
+rePattern = re.compile('[?!\\n]*(.*.)[\\t]*')
+print("Firmware Version:     {}".format(rePattern.findall(dataTemp)[0]))
+
+dataTemp = str(io.StringIO(os.popen( r'''.\\platform-tools\\adb shell getprop ro.build.version.release''').read()).read())
+rePattern = re.compile('[?!\\n]*(.*.)[\\t]*')
+print("Android Version:      {}".format(rePattern.findall(dataTemp)[0]))
+
+dataTemp = str(io.StringIO(os.popen( r'''.\\platform-tools\\adb shell getprop ro.boot.hardware.revision''').read()).read())
+rePattern = re.compile('[?!\\n]*(.*.)[\\t]*')
+print("Hardware Version:     {}".format(rePattern.findall(dataTemp)[0]))
+
+dataTemp = str(io.StringIO(os.popen( r'''.\\platform-tools\\adb shell getprop ro.boot.hardware.ddr''').read()).read())
+rePattern = re.compile('[?!\\n]*(.*.)[\\t]*')
+print("Memory Type:          {}".format(rePattern.findall(dataTemp)[0]))
+
+dataTemp = str(io.StringIO(os.popen( r'''.\\platform-tools\\adb shell getprop ro.product.board''').read()).read())
+rePattern = re.compile('[?!\\n]*(.*.)[\\t]*')
+print("LTE/CDMA Chipset type:{}".format(rePattern.findall(dataTemp)[0]))
+
+dataTemp = str(io.StringIO(os.popen( r'''.\\platform-tools\\adb shell getprop ro.build.expect.baseband''').read()).read())
+rePattern = re.compile('[?!\\n]*(.*.)[\\t]*')
+print("LTE/CDMA Chipset Ver.:{}".format(rePattern.findall(dataTemp)[0]))
